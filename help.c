e652e46c17828cc5aea5b9bd7d9bffb043614c91
Cornfufu <saomai241202@gmail.com>
2022-03-22 18:55:59 +0700

    13->17

---
+++ help.c
@@ -0,0 +1,17 @@
+#include "syscall.h"
+
+int main(){
+    PrintString("\t\t -----GROUP-----\n");
+	PrintString("20127135 - Tran Huynh Ngoc Diep\n");
+	PrintString("20127252 - Ngo Gia Ngan\n\n");
+	PrintString("20127305 - Tran Thao Quyen\n");
+	
+	PrintString("\t\t -----ASCII-----\n");
+	PrintString("Program print ASCII \n\n");
+
+	PrintString("\t\t -----Sort-----\n");
+	PrintString("User can input an array of n integers (n <= 100)\n");
+	PrintString("Using algorithm bubble sort to\nsort the array in ascending\n(descending), depending on user\n\n");
+
+	return 0;
+}
