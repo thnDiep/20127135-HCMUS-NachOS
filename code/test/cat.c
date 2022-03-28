53c6249c636448019998bc2c94a911e0ab593a61
CornCornn <88552189+Cornfufu@users.noreply.github.com>
2022-03-28 20:56:00 +0700

    18->20

---
+++ code/test/cat.c
@@ -0,0 +1,25 @@
+#include "syscall.h" 
+
+int main() {
+    char name[100];
+    PrintString("Input name's length: ");
+    int n = ReadNum();
+    PrintString("\nInput name: ");
+    ReadString(name, n); 
+
+    int k = Open(name);
+    char info;
+    if (k != -1) {
+        int size = Seek(-1, k); // lay do dai noi dung
+        Seek(0,k);              // quay lai dau file de doc noi dung
+        for(int i = 0; i < size; i++) {
+            Read(&info, 1, k);
+            PrintString(info);
+        }
+        Close (k);
+    }
+    else 
+        PrintString ("Error!!";)
+
+    return 0;
+}
